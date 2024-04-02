e7e448f7cb4ce32a1d260dd343a039f13347621b
sejal-112 <161235502+sejal-112@users.noreply.github.com>
2024-04-02 08:23:56 +0000

    Conversion if datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score =  undefined // Nan -> not a number
+//  score = null // 0
+//  score = "345th" // Nan -> not a number
+//  score = true; // gives 1
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1
+// isLoggedIn = ""
+// isLoggedIn = "Sejal"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitest" => true
+
+let someNum = 22
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum)
+
+//
+
+
+
+
+
+
