3fad0ffd10f67326c1bf7b059c36354df6d49024
sejal-112 <161235502+sejal-112@users.noreply.github.com>
2024-04-04 13:01:34 +0000

    Learn conversions of datatypes and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,12 +25,36 @@
 // "" => false
 // "hitest" => true
 
-let someNum = 22
-let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum)
+// let someNum = 22
+// let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum)
 
-//
+// *************************Operations*****************
+let value = 3;
+let negValue = -value
+//console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2)
+console.log(2**2)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "Hello"
+let str2 = "Sejal"
+let str3 = str1+str2
+console.log(str3)
+console.log("1" + 2)
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+
+
+
+
 
 
 
