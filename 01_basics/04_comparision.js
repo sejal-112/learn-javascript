8d1c88579934563d6c244e5501b912477a538a4f
sejal-112 <161235502+sejal-112@users.noreply.github.com>
2024-04-05 09:56:33 +0000

    here we undersatnd about datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+
+// console.log("2" > 1) // automatically js converts "2" into 2
+// console.log("02" > 1)
+
+/* These type of conversions always gives confusion
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// always gives false
+*/
+
+// the most Important comparision operator is strict check
+// ===
+//checks value and aso its datatype
+
+console.log("2" === 2)
+console.log("2" == 2)
---
+++ 01_basics/datatypes.summary.js
@@ -0,0 +1,46 @@
+//  Primitive
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt, 
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+ const outsideTemp = null
+// let userEmail;
+
+
+let id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 343467457234238487923n
+console.log(bigNumber)
+
+
+// Refrence type of Non-prmitive
+// Arrays, Objects, Functions
+
+const heros = ["sahktiman", "naagraj", "doga"]
+let myObj = {
+    name: "sejal",
+    age: 22
+}
+
+const myFunction = function() {
+    console.log("Hello Sejal")
+}
+
+console.log(typeof bigNumber)
+console.log(typeof outsideTemp)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack memory (Primitive types), Heap (Non-Primitive)
+
+let myYoutubename = "sejalgupta.com"
+
+
+
+
+
